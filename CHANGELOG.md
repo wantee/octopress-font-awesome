872f84158d68ad91c785118f1d98166e6e793465
Wang Jian <wantee.wang@gmail.com>
2015-05-17 15:35:31 +0800

    add plugin info

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## 0.1.1 - 2015-05-17
+- Add plugin info in Ruby
+
 ## 0.1.0 - 2015-05-16
 - Initial release
--- lib/octopress-font-awesome.rb
+++ lib/octopress-font-awesome.rb
@@ -7,7 +7,7 @@ Octopress::Ink.add_plugin({
   gem:           "octopress-font-awesome",
   path:          File.expand_path(File.join(File.dirname(__FILE__), "..")),
   version:       OctopressFontAwesome::VERSION,
-  description:   "",                                # What does your theme/plugin do?
-  source_url:    "https://github.com/user/project", # <- Update info
-  website:       ""                                 # Optional project website
+  description:   "Quickly and easily add Font Awesome icons to your posts with octopress ink.", 
+  source_url:    "https://github.com/wantee/octopress-font-awesome/", 
+  website:       "https://github.com/wantee/octopress-font-awesome/"
 })
